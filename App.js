b9a37f92523420df9cb4ba47ea4c9547f50d1a92
Prince <princeitschool@gmail.com>
2022-08-04 04:11:28 -0500

    Expo Publish and Persist Storage

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from "./redux/store";
 import { Provider as PaperProvider} from 'react-native-paper';
 import { StyleSheet } from 'react-native';
+import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./components/LoadingComponent";
 
 const App = () => {
 
 
 
   return (
-      <Provider store={store}>
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
         <PaperProvider>
           <NavigationContainer theme={DefaultTheme}>
             <Main />
           </NavigationContainer>
         </PaperProvider>
-      </Provider>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- redux/store.js
+++ redux/store.js
@@ -8,19 +8,43 @@ import { favoritesReducer } from '../features/favorites/favoritesSlice';
 import { staffReducer } from '../features/staff/staffSlice';
 import { teamsReducer } from '../features/teams/teamSlice';
 import { statsReducer } from '../features/stats/statsSlice';
+import {
+  persistStore,
+  persistCombineReducers,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const config = {
+  key: "root",
+  storage: AsyncStorage,
+  debug: true,
+};
 
 export const store = configureStore({
-    reducer: {
-        campsites: campsitesReducer,
-        comments: commentsReducer,
-        partners: partnersReducer,
-        promotions: promotionsReducer,
-        favorites: favoritesReducer,
-        staff: staffReducer,
-        teams: teamsReducer,
-        stats: statsReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
+  reducer: persistCombineReducers(config, {
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer,
+    favorites: favoritesReducer,
+    staff: staffReducer,
+    teams: teamsReducer,
+    stats: statsReducer,
+  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([logger]),
 });
 
+
+export const persistor = persistStore(store);
 // console.log(store.getState())
